757426caa1e5357849547b1e53ffbce07197c4d3
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-29 00:21:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,6 +43,15 @@ def sum_array(array)
   array.inject(:+)
 end
 
+def add_s(array)
+    array.collect do |word|
+        if array[1] == word
+          word
+        else
+          word + "s"
+        end
+      end
+    end
 
   
   
